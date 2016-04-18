598f42ab33b1b7ae35326a1965f42b839e082265
vgsprasad <vgstech@gmail.com>
2016-04-18 00:33:00 -0700

    Inode bit map corruption correction code working .

--- usr.bin/proj3/inode_bitmap_correction.c
+++ usr.bin/proj3/inode_bitmap_correction.c
@@ -7,11 +7,11 @@
 #include "inode.h"
 #include "super.h"
 #define DIR_NAME_MAX_LEN 1000
+
+int inode_list[10000] ;
+int g_inode_count = 0;
 void dir_walker(char *dir_name);
-void correct_inode_bit_map()
-{
-       dir_walker("/");
-}
+
 void dir_walker(char *dir_name)
 {
     DIR * D;
@@ -42,34 +42,17 @@ void dir_walker(char *dir_name)
 	    {
 		continue;
 	    }
-	    fd = open("/dev/c0d0p1",O_RDWR);
-	    lseek(fd, 1024 + 1024 +dir_entry->d_ino ,SEEK_SET);
-	    ptr = (char *)malloc(sizeof(char));
-	    read(fd,ptr,sizeof(char));
-	    if(!(*ptr & (1 <<((dir_entry->d_ino)%8)))) {
-		val = *ptr | (1 <<(dir_entry->d_ino%8));
-		lseek(fd, 1024+ 1024 +dir_entry->d_ino/8,SEEK_SET);
-		write(fd, &val, sizeof(char));
-		close(fd);
-	    }
+	    inode_list[g_inode_count++] = dir_entry->d_ino;
 	    dir_walker(sub_dir);
 	}
 	else
 	{
-	    fd = open("/dev/c0d0p1",O_RDWR);
-	    lseek(fd, 1024 + 1024 +dir_entry->d_ino,SEEK_SET);
-	    ptr = (char *)malloc(sizeof(char));
-	    read(fd,ptr,sizeof(char));
-	    if(!(*ptr & (1 <<((dir_entry->d_ino)%8)))) {
-		val = *ptr | (1 <<(dir_entry->d_ino%8));
-		lseek(fd, 1024 + 1024 +dir_entry->d_ino/8,SEEK_SET);
-		write(fd, &val, sizeof(char));
-	    }
-	    close(fd);
+	    inode_list[g_inode_count++] = dir_entry->d_ino;
 	}
     }
 }
-int main()
+
+int main(int argc, char **argv)
 {
     char val;
     struct super_block sb;
@@ -77,14 +60,28 @@ int main()
     unsigned long n;
     int count =0;
     int i , j;
+    int inode_num;
     int imapsize, blksize;
     int index;
     char *ptr;
-    struct inode *ptr1;
-    fd = open("/dev/c0d0p1",O_RDONLY);
-    lseek(fd,1024,SEEK_CUR);
+    FILE *fp;
+    char *str=malloc(1024);
+    char *path = malloc(1024);
+    if (argc < 2) {
+	printf("Usage : <binary> <file_name> \n");
+	return 1;
+    }
+    memset(str,0,1024);
+    memset(path,0,1024);
+    sprintf(str,"df %s | grep dev ",argv[1]);
+    fp = popen(str,"r");
+    fscanf(fp,"%s",path);
+
+    dir_walker(".");
+    fd = open(path, O_RDONLY);
+    lseek(fd, 1024, SEEK_CUR);
     n = read(fd,&sb,sizeof(struct super_block ));
-    lseek(fd, 1024 + 1024 ,SEEK_SET);
+    lseek(fd, 1024 * 8 ,SEEK_SET);
 
     imapsize = sb.s_imap_blocks;
     blksize = sb.s_block_size;
@@ -96,8 +93,8 @@ int main()
 	for(j=0;j<8;j++)
 	{
 	    if(*ptr & (0x1 << j)) {
-		printf("%d \t ", i);
 		count++;
+
 	    }
 	}
 	ptr++;
@@ -105,38 +102,25 @@ int main()
     close(fd);
     printf("Total number of inodes allocated =%d \n",count ? count-1:count);
 
-    printf(" Corrupting the first available inode in inode bit map table \n");
-    fd = open("/dev/c0d0p1",O_RDWR);
-    lseek(fd,1024,SEEK_CUR);
-    n = read(fd,&sb,sizeof(struct super_block ));
-    lseek(fd, 1024+ 1024 ,SEEK_SET);
+    fd = open(path, O_RDWR);
+    printf(" Choosing random inode number from the allocated inodes \n");
+    index = rand() % g_inode_count;
+    inode_num = inode_list [index];
+    printf("Chosen inode %d\n", inode_num);
+    printf(" Corrupt that inode \n");
+    lseek(fd, 1024 * 8 + inode_num/8 ,SEEK_SET);
+    ptr = (char *)malloc(sizeof(char));
+    read(fd, ptr, 1);
 
-    imapsize = sb.s_imap_blocks;
-    blksize = sb.s_block_size;
+    val = *ptr & ~(1 << (8- (inode_num % 8)));
 
-    ptr = (char * )malloc(imapsize*blksize);
-    read(fd,ptr,(imapsize*blksize));
-    for(i=0;i<(imapsize*blksize);i++)
-    {
-	for(j=0;j<8;j++)
-	{
-	    if(*ptr & (0x1 << j)) {
-		val = *ptr & ~(0x1 << j);
-		printf("%x \n", *ptr);
-		printf("%x \n", val);
-		lseek(fd, 1024 + 1024 + i ,SEEK_SET);
-	//write(fd, &val,  1);
-		goto done;
-	    }
-	}
-	ptr++;
-    }
-done:
-    printf("One bit in inode bit map is cleared .\n");
+    lseek(fd, 1024 * 8 + inode_num/8 ,SEEK_SET);
+    write(fd, &val,  1);
+
     close(fd);
-    count =0 ;
-    fd = open("/dev/c0d0p1",O_RDONLY);
-    lseek(fd, 1024 + 1024 ,SEEK_SET);
+    count = 0 ;
+    fd = open(path, O_RDONLY);
+    lseek(fd, 1024 * 8 ,SEEK_SET);
     ptr = (char * )malloc(imapsize*blksize);
     read(fd,ptr,imapsize*blksize);
     for(i=0;i<(imapsize*blksize);i++)
@@ -151,6 +135,56 @@ done:
     printf("Total number of inodes allocated =%d \n",count ? (count-1):count);
     printf("\n");
     close(fd);
-    //   correct_inode_bit_map();
+    
+    fd = open(path,O_RDONLY);
+    lseek(fd, 1024*8 ,SEEK_SET);
+
+    ptr = (char * )malloc(imapsize*blksize);
+
+    n = read(fd,ptr,(imapsize*blksize));
+    printf("Inode Bit map is : \n");
+    for(i =0; i< imapsize*blksize; i++)
+    {
+	printf("%x" ,ptr[i]);
+    }
+    printf("\n");
+    printf(" Now correct all the inodes which are broken \n");
+    fd = open(path, O_RDWR);
+    for(i=0; i <g_inode_count; i++)
+    {
+	inode_num = inode_list[i];
+	lseek(fd, 1024 * 8 + inode_num/8 ,SEEK_SET);
+	ptr = (char *)malloc(sizeof(char));
+
+	read(fd, ptr, 1);
+
+	if (*ptr & (1 << (8- (inode_num % 8)))) {
+	    continue;
+	} else {
+
+	    lseek(fd, 1024 * 8 + inode_num/8 ,SEEK_SET);
+	    val = *ptr | (1 << (8- (inode_num % 8)));
+	    write(fd, &val,  1);
+	}
+    }
+    close (fd);
+    count = 0 ;
+    fd = open(path, O_RDONLY);
+    lseek(fd, 1024 * 8 ,SEEK_SET);
+    ptr = (char * )malloc(imapsize*blksize);
+    read(fd,ptr,imapsize*blksize);
+    for(i=0;i<(imapsize*blksize);i++)
+    {
+	for(j=0;j<8;j++)
+	{
+	    if(*ptr & (0x1 << j))
+		count++;
+	}
+
+	ptr++;
+    }
+    printf("Total number of inodes allocated =%d \n",count ? (count-1):count);
+    printf("\n");
+    close(fd);
     return 0;
 }
